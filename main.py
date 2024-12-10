c84d3b146cf193e0d00375efc2788a3dbe59d151
cronox808 <187470821+cronox808@users.noreply.github.com>
2024-12-10 07:25:06 -0500

    Solution exercise 2(completo)

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ a = float(input("ingrese el valor de los lados del triangulo eqilatero: "))
 
 raiz = (math.sqrt(3)/4) * (a** 2)
 
-print(raiz)
+if(raiz >= 1000):
+    print("DATOS NO VÁLIDOS")
+else:
+    print(f"el area del triangulo es {raiz}")
